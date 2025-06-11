28422f1b0f70ba5cfac49094f725cb8f36aceced
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-06-11 14:14:05 +0530

    first commit

--- client/src/components/pages/Whiteboard.jsx
+++ client/src/components/pages/Whiteboard.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Excalidraw } from "@excalidraw/excalidraw";
-// import "excalidraw/dist/excalidraw.min.css";
+import { useState, useRef } from "react";
+import { Excalidraw, Footer } from "@excalidraw/excalidraw";
 
 const Whiteboard = () => {
+  const appRef = useRef(null);
+  const [zenModeEnabled, setZenModeEnabled] = useState(false);
+  const [gridModeEnabled, setGridModeEnabled] = useState(false);
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <Excalidraw />
+    <div className="App" ref={appRef}>
+      <Excalidraw
+        ref={(api) => setExcalidrawAPI(api)}
+        zenModeEnabled={zenModeEnabled}
+        gridModeEnabled={gridModeEnabled}
+        name="Custom Drawing"
+      >
+        {excalidrawAPI && (
+          <Footer>
+            <div>Custom Footer</div>
+          </Footer>
+        )}
+      </Excalidraw>
     </div>
   );
 };
